053a1c67b299996b97ee1de93a4ffec798d29800
Minkyu SHIM <hello.mkshim@gmail.com>
2025-06-13 09:56:43 +0200

    two sum 2 with c sharp

---
+++ two_pointers/two_sum_ii_csharp.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace MyFirstCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+
+        public int[] TwoSum(int[] numbers, int target)
+        {
+            int l = 0;
+            int r = numbers.Length - 1;
+
+            while (l < r)
+            {
+                if (numbers[l] + numbers[r] > target)
+                {
+                    r--;
+                }
+                else if (numbers[l] + numbers[r] < target)
+                {
+                    l++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return [l + 1, r + 1];
+        }
+    }
+}
