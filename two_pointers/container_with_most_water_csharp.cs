dc046e2a9bb5ec55a38462c90b1589e44af8655e
Minkyu SHIM <hello.mkshim@gmail.com>
2025-06-16 20:54:19 +0200

    container with most water with c sharp

---
+++ two_pointers/container_with_most_water_csharp.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace MyFirstCSharp
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+
+            int a = solution.MaxArea(new int[] {1, 8, 6, 2, 5, 4, 8, 3, 7});
+            Console.WriteLine(a);
+
+        }
+    }
+
+    public class Solution
+    {
+        public int MaxArea(int[] height)
+        {
+            int l = 0;
+            int r = height.Length - 1;
+            int maxArea = 0;
+
+            while (l < r)
+            {
+                int currentArea = (r - l) * Math.Min(height[l], height[r]);
+                maxArea = Math.Max(maxArea, currentArea);
+
+                if (height[l] < height[r])
+                {
+                    l++;
+                }
+                else if (height[l] > height[r])
+                {
+                    r--;
+                }
+                else
+                {
+                    if (height[l + 1] > height[r + 1])
+                    {
+                        l++;
+                    }
+                    else
+                    {
+                        r--;
+                    }
+                }
+                
+            }
+            return maxArea;
+        }
+    }
+}
